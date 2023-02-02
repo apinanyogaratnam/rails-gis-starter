0c5dab2e9e2bdfb5aa9b870108c9f2716b70b26e
Apinan Yogaratnam <apinanapinan@icloud.com>
2023-02-02 05:19:03 +0000

    fix: added rating to model

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_043231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.geography "lonlat", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}
+    t.integer "rating", default: 0
   end
 
 end
